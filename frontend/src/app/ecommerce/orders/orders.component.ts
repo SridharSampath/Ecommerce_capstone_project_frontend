04fa6b3da11528ad096d6959eda334716ba4fa1a
SridharSampath <sridharsampath1989@gmail.com>
2019-05-04 20:57:07 +0530

    Add files via upload
    initial commit

---
+++ frontend/src/app/ecommerce/orders/orders.component.ts
@@ -0,0 +1,40 @@
+import {Component, OnInit} from '@angular/core';
+import {ProductOrders} from "../models/product-orders.model";
+import {Subscription} from "rxjs/internal/Subscription";
+import {EcommerceService} from "../services/EcommerceService";
+
+@Component({
+    selector: 'app-orders',
+    templateUrl: './orders.component.html',
+    styleUrls: ['./orders.component.css']
+})
+export class OrdersComponent implements OnInit {
+    orders: ProductOrders;
+    total: number;
+    paid: boolean;
+    sub: Subscription;
+
+    constructor(private ecommerceService: EcommerceService) {
+        this.orders = this.ecommerceService.ProductOrders;
+    }
+
+    ngOnInit() {
+        this.paid = false;
+        this.sub = this.ecommerceService.OrdersChanged.subscribe(() => {
+            this.orders = this.ecommerceService.ProductOrders;
+        });
+        this.loadTotal();
+    }
+
+    pay() {
+        this.paid = true;
+        alert(this.orders);
+        this.ecommerceService.saveOrder(this.orders).subscribe();
+    }
+
+    loadTotal() {
+        this.sub = this.ecommerceService.TotalChanged.subscribe(() => {
+            this.total = this.ecommerceService.Total;
+        });
+    }
+}
